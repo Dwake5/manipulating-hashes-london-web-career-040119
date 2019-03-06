65e8221d47d495aa099e4a1a80a862b1cd3f78c4
Danny Wakeling <wakeling101@hotmail.com>
2019-03-06 15:37:42 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,11 +17,11 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.delete_if do |flavor| flavor == strawberry
+      value.delete_if {|flavor| flavor == "strawberry"}
     end
   end
 end
-end
+
 
   #remember to return your newly altered contacts hash!
   contacts
